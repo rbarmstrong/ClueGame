8f0f18d697b900cd2a7a313819b9370fb8f45974
Andrew Taylor <astaylor@mines.edu>
2020-11-06 14:06:12 -0700

    created testCheckAccusation

---
+++ src/tests/ComputerAITest.java
@@ -0,0 +1,35 @@
+package tests;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import clueGame.Board;
+import clueGame.Card;
+import clueGame.CardType;
+import clueGame.Player;
+
+public class ComputerAITest {
+
+		// We make the Board static because we can load it one time and 
+		// then do all the tests. 
+		private static Board board;
+		
+		@BeforeAll
+		public static void setUp() {
+			// Board is singleton, get the only instance
+			board = Board.getInstance();
+			// set the file names to use my config files
+			board.setConfigFiles("data/ClueLayout.csv", "data/ClueSetup.txt");		
+			// Initialize will load config files 
+			board.initialize();
+		}
+		
+		@Test
+		public void testSelectTargets() {
+			
+		}
+}
